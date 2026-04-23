c252a2d9e2db6d026e0d7367c67a888580ed6285
padaialgo@gmail.com <padaialgo@gmail.com>
2026-04-23 07:16:21 +0000

    commit

--- src/quant_core/ingestion/bronze/__init__.py
+++ src/quant_core/ingestion/bronze/__init__.py
@@ -5,3 +5,22 @@ from quant_core.ingestion.bronze.facts import FACT_BRONZE_SPECS
 
 ALL_BRONZE_SPECS = FACT_BRONZE_SPECS + DIMENSION_BRONZE_SPECS
 
+
+# Utility functions for bronze ingestion
+def resolve_monthly_path(landing_base, dataset, target_yyyymm):
+    """Resolve path to monthly partitioned fact data."""
+    return "{0}/{1}/{2}/*.csv".format(landing_base, dataset, target_yyyymm)
+
+
+def resolve_dimension_path(landing_base, table_name, target_yyyymm):
+    """Resolve path to dimension data for a given month."""
+    return "{0}/dimensions/{1}/{2}.csv".format(landing_base, target_yyyymm, table_name)
+
+
+def build_replace_where(partition_column, partition_values):
+    """Build a replaceWhere condition for Delta table partitions."""
+    ordered_values = sorted(set(partition_values))
+    if not ordered_values:
+        raise ValueError("partition_values must not be empty")
+    quoted_values = ",".join(repr(value) for value in ordered_values)
+    return "{0} IN ({1})".format(partition_column, quoted_values)
